0ca25010697139c2789f34e881a034a538d0029a
phyex0 <halit_burak@outlook.com>
2022-01-08 16:24:15 +0300

    New connection class added, test code added to main.

--- src/main/java/Database.java
+++
@@ -1,8 +0,0 @@
-public class Database {
-    public static final String host = "DESKTOP-HFDS938";
-    //public static final String host = "MONSTER-PC";
-    public static final String userName = "sa";
-    public static final String password = "ekmek";
-    public static final String dbName = "AdventureWorks2019";
-    public static final int port = 1433;
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,54 @@
 import Models.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        connectToDatabase();
+        //Test Code
+        StringBuilder sql = new StringBuilder();
+        sql.append(" SELECT ");
+        sql.append("	DepartmentID, Name, GroupName, ModifiedDate ");
+        sql.append(" FROM HumanResources.Department ");
 
-        DatabaseOperations dbo = new DatabaseOperations();
+        Connection connection = DatabaseUtilities.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql.toString());
+        ResultSet resultSet = preparedStatement.executeQuery();
 
-        ArrayList<User> users = new ArrayList<>();
-        users = dbo.getUsers();
+        while (resultSet.next()) {
+            String name = resultSet.getString("Name");
+            System.out.println(name);
+        }
 
-        if(users != null) {
-            for(User user: users) {
-                System.out.println(user.getFirstName() + " " + user.getLastName() + " " + user.getBusinessEntityId());
-            }
+        System.out.println("Ekmek");
+        disconnectFromDatabase();
+
+    }
+
+    public static void connectToDatabase() {
+        DatabaseUtilities.host = "MONSTER-PC:1433";
+        DatabaseUtilities.databaseName = "AdventureWorks2019";
+        DatabaseUtilities.userName = "sa";
+        DatabaseUtilities.password = "ekmek";
+
+        try {
+            DatabaseUtilities.getConnection();
+        } catch (Exception e) {
+            System.out.println("Exception occured : " + e);
+            return;
+        }
+    }
+
+    public static void disconnectFromDatabase() {
+        try {
+            DatabaseUtilities.disconnect();
+        } catch (Exception e) {
+            System.out.println("Error disconnecting from database : " + e);
+            return;
         }
     }
 }
