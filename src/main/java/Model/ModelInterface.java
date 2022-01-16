38eb1836b08183b22b73e6ae587ac400bf0db10f
phyex0 <halit_burak@outlook.com>
2022-01-16 20:34:26 +0300

    interface updated

--- src/main/java/Model/ModelInterface.java
+++ src/main/java/Model/ModelInterface.java
@@ -1,10 +1,15 @@
 package Model;
 
+import Utility.DatabaseUtilities;
 import Views.ViewData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 
 public interface ModelInterface {
@@ -16,6 +21,33 @@ public interface ModelInterface {
     abstract int update(Map<String, Object> updateParameters, Map<String, Object> whereParameters) throws Exception;
 
     abstract int delete(Map<String, Object> whereParameters) throws Exception;
+
+    static int getRandomId(String tableName) throws Exception {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT");
+        sql.append("    id");
+        sql.append("FROM " + tableName);
+
+        // execute constructed SQL statement
+        Connection connection = DatabaseUtilities.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql.toString());
+        ResultSet result = preparedStatement.executeQuery();
+
+        HashSet<Integer> set = new HashSet<>();
+        while (result.next())
+            set.add(Integer.valueOf(result.getString("id")));
+
+        Random random = new Random();
+        int num = -1;
+        while (true) {
+            num = random.nextInt(Integer.MAX_VALUE);
+            if (!set.contains(num))
+                break;
+        }
+
+        return num;
+
+    }
 
     default ModelData execute(ViewData viewData) throws Exception {
         if (viewData.viewParameters == null) {
